65d0d454d802cf273cb9e92a1c8930b5cb4da2d6
Andrew Caylor <andrewc01@vt.edu>
2020-01-29 22:45:00 -0500

    Slight Refactoring

---
+++ NewSearchResults.js
@@ -0,0 +1,37 @@
+
+//returns a list of the search results scraped from the search page
+function getNewSearchResults() {
+    var out = [];
+
+    var title = document.querySelectorAll('caption')[0].innerHTML
+
+    var elements = document.querySelectorAll('td');
+    for (var i = 13; i < elements.length + 1; i += 2) {
+        if (elements[i]) {
+            out.push(elements[i].innerHTML);
+        }
+    }
+    return { "title": title, "results": out };
+}
+
+//Adds the results to the DOM
+function loadNewSearchResults(results) {
+    var title = document.createElement("h3");
+    title.innerHTML = results.title;
+    document.getElementById("resultsList").appendChild(title);
+
+    results.results.forEach(result => {
+        var toAdd = document.createElement("div");
+
+        document.getElementById("resultsList").appendChild(toAdd);
+
+        var icon = document.createElement("span");
+        icon.setAttribute("uk-icon", "icon: link");
+
+        var link = document.createElement("a");
+        link.setAttribute("class", "uk-link-heading")
+        link.innerHTML = icon.outerHTML + result;
+        toAdd.appendChild(link);
+    });
+
+}
--- injectHTML.js
+++ injectHTML.js
@@ -19,23 +19,22 @@ function load(destURL) {
     }
     else if (destURL == "https://banweb.banner.vt.edu/ssb/prod/twbksrch.P_ShowResults") {
         //search results
-        var results = getSearchResults();
-
-        loadHTML("NewHeader.html", function () { });
-
+        var results = getNewSearchResults();
         document.body.innerHTML = null;
-        loadHTML('NewResults.html', function () {
-            printResults(results);
-        });
 
-        loadHTML("NewFooter.html", function () { });
+        loadHTML("NewHeader.html", function () {            
+            loadHTML('NewResults.html', function () {
+                loadNewSearchResults(results);
+
+                loadHTML("NewFooter.html", function () { });
+            });
+
+        });
     }
     else if (destURL == "https://banweb.banner.vt.edu/ssb/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu" ||
         destURL == "https://banweb.banner.vt.edu/ssb/prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu#") {
 
         document.body.innerHTML = null;
-
-        console.log("teset")
 
         //hokiespa
         loadHTML("NewHeader.html", () => {
@@ -93,43 +92,4 @@ function loadHTML(localURL, callback) {
             document.querySelectorAll("body")[0].appendChild(element);
             callback();
         });
-}
-
-
-//returns a list of the search results scraped from the search page
-function getSearchResults() {
-    var out = [];
-
-    var title = document.querySelectorAll('caption')[0].innerHTML
-
-    var elements = document.querySelectorAll('td');
-    for (var i = 13; i < elements.length + 1; i += 2) {
-        if (elements[i]) {
-            out.push(elements[i].innerHTML);
-        }
-    }
-    return { "title": title, "results": out };
-}
-
-//Adds the results to the DOM
-function printResults(results) {
-
-    var title = document.createElement("h3");
-    title.innerHTML = results.title;
-    document.getElementById("resultsList").appendChild(title);
-
-    results.results.forEach(result => {
-        var toAdd = document.createElement("div");
-
-        document.getElementById("resultsList").appendChild(toAdd);
-
-        var icon = document.createElement("span");
-        icon.setAttribute("uk-icon", "icon: link");
-
-        var link = document.createElement("a");
-        link.setAttribute("class", "uk-link-heading")
-        link.innerHTML = icon.outerHTML + result;
-        toAdd.appendChild(link);
-    });
-
 }
