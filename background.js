b01512f12d0844be0278e3a2e0eee945c3aa8c33
Andrew Caylor <andrewc01@vt.edu>
2020-01-31 22:12:18 -0500

    Added documentation

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-
 //checks when the user reloads the tab and sends a message to the content
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
     if (changeInfo.status == "complete") {
